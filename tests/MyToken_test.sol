0162c0e39cfe646634e6f5167ecac11b2935e70a
Bkyk <2877093878@qq.com>
2024-12-15 17:29:31 +0800

    correct a mistake

---
+++ tests/MyToken_test.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+import "remix_tests.sol";
+import "../contracts/MyToken.sol";
+
+contract MyTokenTest is MyToken {
+
+    function testTokenInitialValues() public {
+        Assert.equal(name(), "MyToken", "token name did not match");
+        Assert.equal(symbol(), "MTK", "token symbol did not match");
+        Assert.equal(decimals(), 18, "token decimals did not match");
+        Assert.equal(totalSupply(), 0, "token supply should be zero");
+    }
+
+    function mint(address to, uint64 value) external {
+        _mint(to,value);
+    }
+
+    function burn(unit256 value) external {
+        _burn(msg.sender, value);
+}
